6455957677deeeeccfb97473dd849af339f6b1c6
sasha <oleksandr.ivaniuk@nure.ua>
2026-04-18 21:27:01 +0300

    Курсова робота початок

---
+++ Form1.Designer.cs
@@ -0,0 +1,358 @@
+﻿namespace Курсова_Робота__Щоденник_
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            panel1 = new Panel();
+            button1 = new Button();
+            panel4 = new Panel();
+            label7 = new Label();
+            label8 = new Label();
+            pictureBox3 = new PictureBox();
+            panel5 = new Panel();
+            label9 = new Label();
+            label10 = new Label();
+            pictureBox4 = new PictureBox();
+            panel3 = new Panel();
+            label5 = new Label();
+            label6 = new Label();
+            pictureBox2 = new PictureBox();
+            panel2 = new Panel();
+            label4 = new Label();
+            label3 = new Label();
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            label2 = new Label();
+            panel1.SuspendLayout();
+            panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            panel5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Anchor = AnchorStyles.None;
+            panel1.BackColor = Color.FromArgb(227, 220, 222);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(panel4);
+            panel1.Controls.Add(panel5);
+            panel1.Controls.Add(panel3);
+            panel1.Controls.Add(panel2);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(label2);
+            panel1.Location = new Point(78, 39);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1366, 1071);
+            panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Anchor = AnchorStyles.None;
+            button1.BackColor = Color.FromArgb(202, 196, 207);
+            button1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            button1.ForeColor = SystemColors.ActiveCaptionText;
+            button1.Location = new Point(636, 949);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 12;
+            button1.Text = "Далі";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // panel4
+            // 
+            panel4.BackColor = Color.FromArgb(229, 225, 230);
+            panel4.Controls.Add(label7);
+            panel4.Controls.Add(label8);
+            panel4.Controls.Add(pictureBox3);
+            panel4.Location = new Point(752, 692);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(495, 115);
+            panel4.TabIndex = 11;
+            // 
+            // label7
+            // 
+            label7.Anchor = AnchorStyles.None;
+            label7.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label7.ForeColor = Color.FromArgb(77, 0, 18);
+            label7.Location = new Point(129, 65);
+            label7.Name = "label7";
+            label7.Size = new Size(308, 48);
+            label7.TabIndex = 8;
+            label7.Text = "Аналіз можливих накладок між подіями";
+            label7.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // label8
+            // 
+            label8.Anchor = AnchorStyles.None;
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label8.ForeColor = Color.FromArgb(35, 0, 43);
+            label8.Location = new Point(129, 17);
+            label8.Name = "label8";
+            label8.Size = new Size(170, 38);
+            label8.TabIndex = 7;
+            label8.Text = "Неполадки";
+            label8.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Anchor = AnchorStyles.None;
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(18, 17);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(91, 75);
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.TabIndex = 0;
+            pictureBox3.TabStop = false;
+            // 
+            // panel5
+            // 
+            panel5.BackColor = Color.FromArgb(229, 225, 230);
+            panel5.Controls.Add(label9);
+            panel5.Controls.Add(label10);
+            panel5.Controls.Add(pictureBox4);
+            panel5.Location = new Point(110, 692);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(495, 115);
+            panel5.TabIndex = 10;
+            // 
+            // label9
+            // 
+            label9.Anchor = AnchorStyles.None;
+            label9.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label9.ForeColor = Color.FromArgb(77, 0, 18);
+            label9.Location = new Point(136, 65);
+            label9.Name = "label9";
+            label9.Size = new Size(321, 36);
+            label9.TabIndex = 8;
+            label9.Text = "Організовуйте свої нотатки";
+            label9.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // label10
+            // 
+            label10.Anchor = AnchorStyles.None;
+            label10.AutoSize = true;
+            label10.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label10.ForeColor = Color.FromArgb(35, 0, 43);
+            label10.Location = new Point(146, 17);
+            label10.Name = "label10";
+            label10.Size = new Size(302, 38);
+            label10.TabIndex = 7;
+            label10.Text = "Керування записами";
+            label10.TextAlign = ContentAlignment.TopCenter;
+            label10.Click += label10_Click;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Anchor = AnchorStyles.None;
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(21, 28);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(91, 75);
+            pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox4.TabIndex = 0;
+            pictureBox4.TabStop = false;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.FromArgb(229, 225, 230);
+            panel3.Controls.Add(label5);
+            panel3.Controls.Add(label6);
+            panel3.Controls.Add(pictureBox2);
+            panel3.Location = new Point(752, 390);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(495, 115);
+            panel3.TabIndex = 9;
+            // 
+            // label5
+            // 
+            label5.Anchor = AnchorStyles.None;
+            label5.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label5.ForeColor = Color.FromArgb(77, 0, 18);
+            label5.Location = new Point(129, 58);
+            label5.Name = "label5";
+            label5.Size = new Size(321, 48);
+            label5.TabIndex = 8;
+            label5.Text = "Навчання, зустрічі або робота";
+            label5.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // label6
+            // 
+            label6.Anchor = AnchorStyles.None;
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label6.ForeColor = Color.FromArgb(35, 0, 43);
+            label6.Location = new Point(129, 10);
+            label6.Name = "label6";
+            label6.Size = new Size(191, 38);
+            label6.TabIndex = 7;
+            label6.Text = "Нагадування";
+            label6.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Anchor = AnchorStyles.None;
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(18, 21);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(91, 75);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 0;
+            pictureBox2.TabStop = false;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.FromArgb(229, 225, 230);
+            panel2.Controls.Add(label4);
+            panel2.Controls.Add(label3);
+            panel2.Controls.Add(pictureBox1);
+            panel2.Location = new Point(110, 390);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(495, 115);
+            panel2.TabIndex = 3;
+            // 
+            // label4
+            // 
+            label4.Anchor = AnchorStyles.None;
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label4.ForeColor = Color.FromArgb(77, 0, 18);
+            label4.Location = new Point(146, 58);
+            label4.Name = "label4";
+            label4.Size = new Size(321, 48);
+            label4.TabIndex = 8;
+            label4.Text = "Додавання, редагування справ";
+            label4.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // label3
+            // 
+            label3.Anchor = AnchorStyles.None;
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label3.ForeColor = Color.FromArgb(35, 0, 43);
+            label3.Location = new Point(146, 10);
+            label3.Name = "label3";
+            label3.Size = new Size(269, 38);
+            label3.TabIndex = 7;
+            label3.Text = "Планування справ";
+            label3.TextAlign = ContentAlignment.TopCenter;
+            label3.Click += label3_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Anchor = AnchorStyles.None;
+            pictureBox1.Image = Properties.Resources.brandPlan;
+            pictureBox1.Location = new Point(21, 21);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(91, 75);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.None;
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label1.ForeColor = Color.FromArgb(35, 0, 43);
+            label1.Location = new Point(388, 222);
+            label1.Name = "label1";
+            label1.Size = new Size(651, 46);
+            label1.TabIndex = 1;
+            label1.Text = "Ви ввійшли в електронний щоденник";
+            label1.TextAlign = ContentAlignment.TopCenter;
+            label1.Click += label1_Click_1;
+            // 
+            // label2
+            // 
+            label2.Anchor = AnchorStyles.None;
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 25.8000011F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label2.ForeColor = Color.FromArgb(35, 0, 43);
+            label2.Location = new Point(480, 61);
+            label2.Name = "label2";
+            label2.Size = new Size(447, 60);
+            label2.TabIndex = 0;
+            label2.Text = "Вітаю користувачу!";
+            label2.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(255, 237, 243);
+            ClientSize = new Size(1527, 1153);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel4.ResumeLayout(false);
+            panel4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            panel5.ResumeLayout(false);
+            panel5.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label2;
+        private Label label1;
+        private Panel panel2;
+        private PictureBox pictureBox1;
+        private Label label3;
+        private Label label4;
+        private Panel panel3;
+        private Label label5;
+        private Label label6;
+        private PictureBox pictureBox2;
+        private Panel panel4;
+        private Label label7;
+        private Label label8;
+        private PictureBox pictureBox3;
+        private Panel panel5;
+        private Label label9;
+        private Label label10;
+        private PictureBox pictureBox4;
+        private Button button1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,30 @@
+namespace Курсова_Робота__Щоденник_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
